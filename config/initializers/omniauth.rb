7b6a99e0d2b1f70074b2eba0998fb44606d93d1b
Rodrigo Saito <rodrigo.saito@gmail.com>
2016-05-21 21:07:49 -0300

    changed shopify credentials

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :shopify, "3c27a6baf6112b449967ee0e90cc7865", "31d0751ffb884b74591faf4a261fc1a9", :scope => 'read_products,write_products'
+  provider :shopify, "74e9ee24cf26519b7297f77fff2b9ac3", "8b5b5ccb9fe186ff98c0ead1d318a45a", :scope => 'read_products,write_products'
 end
